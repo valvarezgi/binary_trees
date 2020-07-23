ac7603751c193b46ce8c9853f48ceb55699bc8ab
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-07-23 11:57:16 -0500

    [Fix] Style issues

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -44,7 +44,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_perfect - function that checks if a 
+ * binary_tree_is_perfect - function that checks if a
  * binary tree is perfect
  * @tree: pointer to the root of the tree to check
  * Return: 0 if tree is NULL
@@ -52,18 +52,17 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    if (!binary_tree_balance(tree))
-    {
-        if (!binary_tree_height(tree->right) && !binary_tree_height(tree->left))
-            return (1);
-        
-        else
-            return (binary_tree_is_perfect(tree->right) && binary_tree_is_perfect(tree->left));
-        
-    }
-    else
-        return (0);
-    
+	if (!tree)
+		return (0);
+	if (!binary_tree_balance(tree))
+	{
+		if (!binary_tree_height(tree->right)
+		    && !binary_tree_height(tree->left))
+			return (1);
+		else
+			return (binary_tree_is_perfect(tree->right)
+				&& binary_tree_is_perfect(tree->left));
+	}
+	else
+		return (0);
 }
